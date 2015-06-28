cdbaee3d112a60421295cbc579cc931af2f7804c
Mahisorn Wongphati <mahisorn.w@hiveground.com>
2015-06-28 22:34:04 +0700

    added parameters

--- src/3dm_gx4_node.cpp
+++ src/3dm_gx4_node.cpp
@@ -14,14 +14,12 @@ using namespace std;
 
 ros::Publisher g_pub_imu;
 ros::Publisher g_pub_ef_imu;
+sensor_msgs::Imu g_imu;
 
 void publishIMUData(const hg_3dm_gx4::IMUData& data)
 {
-  //cout << __FUNCTION__ << endl;
-  sensor_msgs::Imu imu;
+  g_imu.header.stamp = ros::Time::now();
 
-  imu.header.stamp = ros::Time::now();
-  imu.header.frame_id = "imu";
 
   tf::Quaternion q(data.orientation_quaternion[1],
                    data.orientation_quaternion[2],
@@ -34,42 +32,39 @@ void publishIMUData(const hg_3dm_gx4::IMUData& data)
 
   q.setRPY(roll, -pitch, -yaw);
 
-  imu.orientation.w = q.w();
-  imu.orientation.x = q.x();
-  imu.orientation.y = q.y();
-  imu.orientation.z = q.z();
+  g_imu.orientation.w = q.w();
+  g_imu.orientation.x = q.x();
+  g_imu.orientation.y = q.y();
+  g_imu.orientation.z = q.z();
 
 
-  imu.linear_acceleration.x = data.scaled_accelerometer[0];
-  imu.linear_acceleration.y = -data.scaled_accelerometer[1];
-  imu.linear_acceleration.z = -data.scaled_accelerometer[2];
-  imu.angular_velocity.x = data.scaled_gyro[0];
-  imu.angular_velocity.y = -data.scaled_gyro[1];
-  imu.angular_velocity.z = -data.scaled_gyro[2];
+  g_imu.linear_acceleration.x = data.scaled_accelerometer[0];
+  g_imu.linear_acceleration.y = -data.scaled_accelerometer[1];
+  g_imu.linear_acceleration.z = -data.scaled_accelerometer[2];
+  g_imu.angular_velocity.x = data.scaled_gyro[0];
+  g_imu.angular_velocity.y = -data.scaled_gyro[1];
+  g_imu.angular_velocity.z = -data.scaled_gyro[2];
 
-  g_pub_imu.publish(imu);
+  g_pub_imu.publish(g_imu);
 }
 
 void publishEFData(const hg_3dm_gx4::EFData& data)
 {
-  //cout << __FUNCTION__ << endl;
-  sensor_msgs::Imu imu;
+  g_imu.header.stamp = ros::Time::now();
 
-  imu.header.stamp = ros::Time::now();
-  imu.header.frame_id = "imu";
 
-  imu.orientation.w = data.orientation_quaternion[0];
-  imu.orientation.x = data.orientation_quaternion[1];
-  imu.orientation.y = data.orientation_quaternion[2];
-  imu.orientation.z = data.orientation_quaternion[3];
-  imu.linear_acceleration.x = data.gravity_vector[0];
-  imu.linear_acceleration.y = data.gravity_vector[1];
-  imu.linear_acceleration.z = data.gravity_vector[2];
-  imu.angular_velocity.x = data.compensated_angular_rate[0];
-  imu.angular_velocity.y = data.compensated_angular_rate[1];
-  imu.angular_velocity.z = data.compensated_angular_rate[2];
+  g_imu.orientation.w = data.orientation_quaternion[0];
+  g_imu.orientation.x = data.orientation_quaternion[1];
+  g_imu.orientation.y = data.orientation_quaternion[2];
+  g_imu.orientation.z = data.orientation_quaternion[3];
+  g_imu.linear_acceleration.x = data.gravity_vector[0];
+  g_imu.linear_acceleration.y = data.gravity_vector[1];
+  g_imu.linear_acceleration.z = data.gravity_vector[2];
+  g_imu.angular_velocity.x = data.compensated_angular_rate[0];
+  g_imu.angular_velocity.y = data.compensated_angular_rate[1];
+  g_imu.angular_velocity.z = data.compensated_angular_rate[2];
 
-  g_pub_imu.publish(imu);
+  g_pub_imu.publish(g_imu);
 }
 
 int main(int argc, char **argv)
@@ -79,19 +74,40 @@ int main(int argc, char **argv)
 
   hg_3dm_gx4::Hg3dmGx4 imu;
 
-  if(!imu.openPort("/dev/ttyACM0", 460800))
+  std::string topic;
+  std::string device;
+  int baudrate;
+  std::string frame_id;
+  int imu_rate;
+
+  nh.param<std::string>("topic", topic,"/imu/data");
+  nh.param<std::string>("device", device,"/dev/ttyACM0");
+  nh.param<std::string>("frame_id", g_imu.header.frame_id, "microstrain_link");
+  nh.param<int>("baudrate", baudrate, 460800);
+  nh.param<int>("imu_rate", imu_rate, 500);
+
+  if(!imu.openPort(device, baudrate))
   {
-    ROS_ERROR("Cannot open device");
+    ROS_ERROR_STREAM("Cannot open : " << device);
     return -1;
   }
 
   imu.ping();
-
   imu.idle();
-
   imu.selectBaudRate(460800);
 
-  static const int decimation = (500/100);
+  if(imu_rate > 500)
+  {
+    ROS_WARN("Max frequency is 500!");
+    imu_rate = 500;
+  }
+  else if(imu_rate < 0)
+  {
+    ROS_WARN("Min frequency should be 1! 100 Hz will be set");
+    imu_rate = 100;
+  }
+
+  static const int decimation = (500/imu_rate);
 
   imu.setIMUDataRate(decimation, //(500 / 1) for 3DM-GX4-45
                      hg_3dm_gx4::IMUData::SCALED_ACCELEROMETER |
@@ -129,7 +145,7 @@ int main(int argc, char **argv)
 
 
 
-  g_pub_imu = nh.advertise<sensor_msgs::Imu>("/imu/data", 1);
+  g_pub_imu = nh.advertise<sensor_msgs::Imu>(topic, 1);
 
   imu.setIMUDataCallback(publishIMUData);
   //imu.setEFDataCallback(publishEFData);
